11d8685e0dd226bf703d7900ed81483ba640f11a
zongyan1005 <zongyan1005@gmail.com>
2024-12-25 14:29:19 +0800

    1231

--- vue.js
+++ vue.js
@@ -1,11 +1,29 @@
+
+
 const {createApp,ref}  = Vue;
 
 var Gameplaydata = createApp({
     data() {
         return{
             Gameplay:[
-                {text:"玩家需要通過不同的關卡，找出相對應的鑰匙，逃出迷宮。",}
+                {title:"玩家需要通過不同的關卡，找出相對應的鑰匙，逃出迷宮。",
+                text1:"門上會有不同的顏色，玩家需要找出鑰匙拼湊出門上的顏色，開啟門前往下一關。",
+                text2:"在路上玩家不只要尋找鑰匙，還要躲避鬼魂的攻擊，鬼魂依據關卡的不同速度與數量都會增加。",
+                text3:"玩家要在生命殆盡前逃出迷宮。"
+                }
             ]
         };
     }
 }).mount("#gameplay");
+
+var Galleryplaydata = createApp({
+    data() {
+        return{
+            Gallery:[
+                {imgSrc:"play1.png"},
+                {imgSrc:"play2.png"},
+                {imgSrc:"play2.png"}
+            ]
+        };
+    }
+}).mount("#gallery");
